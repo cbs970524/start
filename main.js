51c1e99a998d42a24f5556bc699c9af64da6e209
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2020-02-08 14:45:06 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs=require('fs');
 var url=require('url');
+var qs=require('querystring');
 function templateHTML(title,list,body){
   return `
   <!DOCTYPE html>
@@ -9,13 +10,13 @@ function templateHTML(title,list,body){
     <title>sail - ${title}</title>
     <meta charset="utf-8">
   </head>
-  ​
   <body>
     <h1><a href="/">홈</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
-  </html>`;
+  </html>`
 }
 function templateList(filelist){
   var list='<ul>';
@@ -53,6 +54,40 @@ var app = http.createServer(function(request,response){
         });
       });
     }
+  }else if(pathname==='/create'){
+    fs.readdir('./data','utf8',function(error,filelist){
+      var title='WEB-create';
+      var list=templateList(filelist);
+      var html=templateHTML(title,list,`
+        <form action="/create_process" method="post">
+          <p>
+            <input type="text" name="title" placeholder="title">
+          </p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit" value="create"
+          </p>
+        </form>
+      `);
+      response.writeHead(200);
+      response.end(html);
+    });
+  }else if(pathname==='/create_process'){
+    var body='';
+    request.on('data',function(date){
+      body=body+data;
+    });
+    request.on('end',function(){
+      var post =qs.parse(body);
+      var title=post.title;
+      var description=post.description;
+      fs.writeFile(`data/${title}`,description,function(err){
+        reponse.writeHead(302,{Location: `/?id=${title}`});
+        response.end();
+      })
+    });
   }else{
     response.writeHead(404);
     response.end('Not found');
