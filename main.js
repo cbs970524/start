3809c0914c38180586e1886bc7e54aeb1914a6d3
cbs970524 <58094664+cbs970524@users.noreply.github.com>
2020-02-09 02:49:23 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs=require('fs');
 var url=require('url');
 var qs=require('querystring');
-function templateHTML(title,list,body){
+function templateHTML(title,list,body,controls){
   return `
   <!DOCTYPE html>
   <html>
@@ -13,10 +13,10 @@ function templateHTML(title,list,body){
   <body>
     <h1><a href="/">홈</a></h1>
     ${list}
-    <a href="/create">create</a>
+    ${controls}
     ${body}
   </body>
-  </html>`
+  </html>`;
 }
 function templateList(filelist){
   var list='<ul>';
@@ -32,14 +32,13 @@ var app = http.createServer(function(request,response){
   var _url=request.url;
   var queryData=url.parse(_url,true).query;
   var pathname=url.parse(_url,true).pathname;
-  var title=queryData.id;
   if(pathname==='/'){
     if(queryData.id===undefined){
       fs.readdir('./data','utf8',function(error,filelist){
         var title='홈';
         var description='Hello world';
         var list=templateList(filelist);
-        var html = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+        var html = templateHTML(title,list,`<h2>${title}</h2>${description}`,`<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(html);
       });
@@ -48,7 +47,8 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${queryData.id}`,'utf8',function(error,description){
           var title=queryData.id;
           var list=templateList(filelist);
-          var html = templateHTML(title,list,`<h2>${title}</h2>${description}`);
+          var html = templateHTML(title,list,`<h2>${title}</h2>${description}`,
+            `<a href="/create">create</a> | <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(html);
         });
@@ -67,7 +67,7 @@ var app = http.createServer(function(request,response){
             <textarea name="description" placeholder="description"></textarea>
           </p>
           <p>
-            <input type="submit" value="create"
+            <input type="submit" value="create">
           </p>
         </form>
       `);
@@ -76,17 +76,17 @@ var app = http.createServer(function(request,response){
     });
   }else if(pathname==='/create_process'){
     var body='';
-    request.on('data',function(date){
-      body=body+data;
+    request.on('data',function(data){
+      body=body + data;
     });
     request.on('end',function(){
       var post =qs.parse(body);
       var title=post.title;
       var description=post.description;
       fs.writeFile(`data/${title}`,description,function(err){
-        reponse.writeHead(302,{Location: `/?id=${title}`});
+        response.writeHead(302,{Location: `/?id=${title}`});
         response.end();
-      })
+      });
     });
   }else{
     response.writeHead(404);
